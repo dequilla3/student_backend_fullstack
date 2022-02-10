be7a69412d0421c039e23f5ef52d358cd9f26dab
Kim Dequilla <67781364+dequilla3@users.noreply.github.com>
2022-02-11 00:34:42 +0800

    Initial commit

---
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.student.controller;
+
+import com.example.student.model.Student;
+import com.example.student.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/student")
+@CrossOrigin
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String addStudent(@RequestBody Student student){
+        studentService.saveStudent(student);
+
+        return "New student added!";
+    }
+
+    @GetMapping("/getAllStudents")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+}
---
+++ src/main/java/com/example/student/service/StudentServiceImplementation.java
@@ -0,0 +1,24 @@
+package com.example.student.service;
+
+import com.example.student.model.Student;
+import com.example.student.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImplementation implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+}
